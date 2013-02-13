df10c04a7994d0b74171ebf65a601638040a18c9
Zach Schneider <me@zachschneider.com>
2013-02-13 15:03:44 -0600

    inserting into my local DB

--- day3.php
+++ day3.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html>
  <head>
-	<title>Sample Page</title>
+	<title>Form</title>
 	<meta charset="UTF-8" />
+	<style type="text/css">
+		body {
+			font-family: "Lucida Console", "Lucida Sans Typewriter", Monaco, "Bitstream Vera Sans Mono", monospace;
+			font-size:14px;
+			line-height:1.5em;
+		}
+		
+	</style>
  </head>
  <body>
-
+	<h1 style="display:none; color:red;">You don't have Kids</h1>
+	<form action="process.php" action="get">
+		<div id="kids">
+			<div>Do you have kids?</div>
+			<label for="kd_n">No</label> <input id="kd_n" type='radio' name='kids' value='0' checked />
+			<label for="kd_y">Yes</label> <input id="kd_y" type='radio' name='kids' value='1' />
+		</div>
+		<div id="numKids">
+			<div>How old is your oldest child</div>
+			<input type="text" name="age" placeholder="child's age" />
+		</div>
+		<input type="submit" value="Submit" name="submit" />
+	</form>
  </body>
 </html>
---
+++ day3/albums.php
@@ -0,0 +1,74 @@
+<?php
+	// connect to database server
+	$dbLink = @mysql_connect('localhost','root','xampp');
+	if (!$dbLink) exit('<p><strong>Error:</strong> Connecting to database server.</p>');
+	
+	// select database
+	if (!@mysql_select_db('tc5774')) exit('<p><strong>Error:</strong> Selecting database.');
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+	<title>MySQL QUERY Form</title>
+	<meta charset="UTF-8" />
+	<style type="text/css">
+		body {
+			font-family: "Lucida Console", "Lucida Sans Typewriter", Monaco, "Bitstream Vera Sans Mono", monospace;
+			font-size:14px;
+			line-height:1.5em;
+		}	
+	</style>
+ </head>
+ <body>
+ <?php 
+	// is form data present?
+	if (!empty($_POST)) {
+		// process form data
+		
+		
+		// build query
+		$title = $_POST['title'];
+		$description = $_POST['description'];
+		
+		$query = 'INSERT INTO albums SET ' . 
+					"title = '$title', " .
+					"description = '$description' ";
+		
+		// send query to server
+		$result = @mysql_query($query);
+		
+		// check query result
+		if (!$result) {
+			// query error - display debug or friendly message
+			echo '<p><strong>Query error</strong> - no soup for you! *snap*</p>';
+		} else {
+			// success!
+			echo '<p>The record has been saved.</p>';
+		}		
+		
+		// process results if any
+		
+		
+		echo "thank you have a nice day!";
+	} else {
+		//display blank form
+	?>
+	<form action="albums.php" method="post">
+		<label>
+			<span>Album Title:</span>
+			<input type="text" name="title" />
+		</label>
+		<label>
+			<span>Album Description:</span>
+			<input type="text" name="description" />
+		</label>
+		<div>
+			<input type="submit" value="Save Changes" />
+		</div>
+	</form>
+<?php
+	} // if form data present
+?>
+ </body>
+</html>
